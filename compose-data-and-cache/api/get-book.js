aaddfba3fb06f3e1a3ed4a20a693ff8782410520
tripott <tripott@gmail.com>
2021-11-05 19:26:05 +0000

    wip api

---
+++ compose-data-and-cache/api/get-book.js
@@ -0,0 +1,28 @@
+import connect from 'hyper-connect'
+const hyper = connect(process.env['HYPER'])()
+
+const readFromDB = id => hyper.data.get(id) 
+
+const isDocCached = id => hyper.cache.get(id)
+  .then(result => {
+    const isDocInCache = result.id === id
+    console.log(`retrieved doc ${id} from cache? ${isDocInCache}`)
+    return result.ok === false ? Promise.reject(id) : result
+  })
+
+const passValueThru = x => x
+const errorResponse = err => {ok: false, err}
+
+// get - fallback to data if not in cache
+const get = id => 
+  Promise.resolve(id)
+    .then(isDocCached)
+    .then(passValueThru, readFromDB)
+    .catch(errorResponse)
+
+export default async function (req, res) {
+
+  console.log('getting book: ', req.params.id)
+  const book = await get(req.params.id)
+  return res.send(book)
+}
---
+++ compose-data-and-cache/api/update-book.js
@@ -0,0 +1,29 @@
+import connect from 'hyper-connect'
+const hyper = connect(process.env['HYPER'])()
+
+const passValueThru = x => x
+
+const updateDocToDB = doc => hyper.data.update(doc)
+  .then(res => {
+    console.log('updateDocToDB res', res)
+    return res.ok ? doc : Promise.reject(res)
+  })
+
+const updateDocToCache = doc => hyper.cache.set(doc.id, doc)
+  .then(passValueThru)
+
+const errorResponse = err => {ok: false, err}
+
+// cacheUpdate - update doc to data and cache 
+const cacheUpdate = doc => Promise.resolve(doc)
+    .then(updateDocToDB)
+    .then(updateDocToCache, passValueThru)
+    .catch(errorResponse)
+
+export default async function (req, res) {
+
+     console.log('update-book', req.body)
+
+    const result = await cacheUpdate(req.body)
+    return res.send(result)
+  }

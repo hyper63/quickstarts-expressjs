9f5e6aba0531b451bcc2ac60204e0a12066b4b9f
tripott <tripott@gmail.com>
2021-11-18 14:57:35 +0000

    data/get

--- hyper-vision-api/server.js
+++ hyper-vision-api/server.js
@@ -1,24 +1,24 @@
 import express from 'express'
 import cors from 'cors'
 
-import getBookFromDB from './api/get-book.js'
-import createBook from './api/create-book.js'
-import deleteBook from './api/delete-book.js'
-import updateBook from './api/update-book.js'
-import searchByAuthor from './api/search-by-author.js'
+import getBookFromDB from './api/data/get.js'
+// import createBook from './api/create-book.js'
+// import deleteBook from './api/delete-book.js'
+// import updateBook from './api/update-book.js'
+// import searchByAuthor from './api/search-by-author.js'
 
-import getBookFromSearch from './api/get-book-search.js'
+// import getBookFromSearch from './api/get-book-search.js'
 const app = express()
 
 app.use(cors())
 app.use(express.json())
 
-app.get('/api/books/_search', searchByAuthor)
+// app.get('/api/books/_search', searchByAuthor)
 
-app.post('/api/books', createBook)
-app.get('/api/books/:id', getBookFromSearch)
-app.delete('/api/books/:id', deleteBook)
-app.put('/api/books/:id', updateBook)
+// app.post('/api/books', createBook)
+app.get('/api/books/:id', getBookFromDB)
+// app.delete('/api/books/:id', deleteBook)
+// app.put('/api/books/:id', updateBook)
 
 app.get('/', function (req, res) {
   res.send({
@@ -27,4 +27,6 @@ app.get('/', function (req, res) {
   })
 })
 
-app.listen(3333)
+const PORT = 3001
+
+app.listen(PORT, null, () => console.log(`listening on PORT: ${PORT}`))
